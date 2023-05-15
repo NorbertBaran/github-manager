2860751beca82035c4e432eada5caff0bf8a45c4
Norbert Baran <norbertbaran1@gmail.com>
2023-05-15 13:33:54 +0200

    Syntax fix

--- .ghm/list.sh
+++ .ghm/list.sh
@@ -2,9 +2,9 @@
 
 if [ $# -eq 0 ]; then
     gh repo list --limit 1000 | awk '{print $1;}'
-elif [ $# -eq 1 ] && [[ $1 == "--public" || $1 == "--private"]]
+elif [ $# -eq 1 ] && [[ $1 == "--public" || $1 == "--private" ]]; then
     gh repo list $1 --limit 1000 | awk '{print $1;}'
-elif [ $# -eq 1 ]
+elif [ $# -eq 1 ]; then
     gh repo list --limit 1000 | awk '{print $1;}' > $1
 elif [ $# -eq 2 ]; then
     gh repo list $2 --limit 1000 | awk '{print $1;}' > $1
